b40de7feca838b400d52e55a04296487cd8aadaf
Jesus Bernal <jbbernal971220@gmail.com>
2023-12-21 21:36:12 -0500

    two commit

--- assets/javascript/juego.js
+++ assets/javascript/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+//*esta funcion crea un nuevo deck
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let tipo of tipos) {
@@ -22,9 +23,28 @@ const crearDeck = () => {
     }
   }
 
-  console.log(deck);
   deck = _.shuffle(deck);
   console.log(deck);
+  return deck;
 };
 
 crearDeck();
+
+//*Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw 'No hay cartas en el deck';
+  }
+
+  const carta = deck.pop();
+
+  return carta;
+};
+
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+
+  return isNaN(valor) ? (valor === 'A' ? 11 : 10) : valor * 1;
+};
+
+console.log(valorCarta(pedirCarta()));
